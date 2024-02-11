8fcb776fdb1c2ffd995de5a704bcbef6e140a32b
Anton Shilin <anton.d.shilin@gmail.com>
2024-02-11 15:29:09 +0300

    FIx run address

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -8,7 +8,7 @@ var RunAddr string
 var BaseAddr string
 
 func ParseFlags() {
-	flag.StringVar(&RunAddr, "a", "localhost:8080", "address and port to run server")
+	flag.StringVar(&RunAddr, "a", "localhost:8080/", "address and port to run server")
 	flag.StringVar(&BaseAddr, "b", "http://localhost:8080/", "base URL before short link")
 	flag.Parse()
 }
